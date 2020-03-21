76ef340fab65ad9488b31fb18763fe701a66a1a5
Shashank  Shahare <srshahare22@gmail.com>
2020-03-21 04:29:34 -0700

    Updates

---
+++ precache-manifest.f9c011b177145a708bcee46fd879dc1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a92bc924e3636854c9bf8a1b8704b5b",
+    "url": "/faceDetection_reactApp/index.html"
+  },
+  {
+    "revision": "ac93276d47a0a276a886",
+    "url": "/faceDetection_reactApp/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "4d822231493ebe9f8b17",
+    "url": "/faceDetection_reactApp/static/css/main.0ed21553.chunk.css"
+  },
+  {
+    "revision": "ac93276d47a0a276a886",
+    "url": "/faceDetection_reactApp/static/js/2.769460a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/faceDetection_reactApp/static/js/2.769460a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d822231493ebe9f8b17",
+    "url": "/faceDetection_reactApp/static/js/main.ce806aee.chunk.js"
+  },
+  {
+    "revision": "89a1a69a7db01017bdb0",
+    "url": "/faceDetection_reactApp/static/js/runtime-main.7ca4bcda.js"
+  }
+]);
