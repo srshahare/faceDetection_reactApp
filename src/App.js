e2161fb3acbb7b30d428e35e1779d80fd94a6094
Shashank  Shahare <srshahare22@gmail.com>
2020-03-22 13:07:32 -0700

    changes design and added server side to app

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ class App extends Component {
         this.state.input)
       .then (response => {
         if(response){
-          fetch('http://localhost:3000/image', {
+          fetch('https://pacific-stream-02466.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
